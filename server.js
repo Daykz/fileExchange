d79a8e18d0d3d20ad98886e0e490de6cbfa5d792
Lu <lubaujar@student.42.fr>
2018-02-05 13:45:12 +0100

    Update server.js

--- server.js
+++ server.js
@@ -2,6 +2,7 @@ const express    = require('express');
 const app        = express();
 const bodyParser = require('body-parser');
 const cors	   	= require('cors');
+   // pour l'instant pas besoin de cors()
 const fs 		= require("fs");
 
 
@@ -21,7 +22,7 @@ app.use(bodyParser.urlencoded({ extended: true }))
    .use(bodyParser.json())
    .use(cors());
 
-
+// essaye de tjrs declarer tes variabkles en haut du fichier
 const port = process.env.PORT || 5000;
 
 app.listen(port);
