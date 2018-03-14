d13554419283daadffd7bd7fdf6f82549967e2a2
Daykz <daykz>
2018-03-14 15:09:36 +0100

    async/await

--- script/request.js
+++ script/request.js
@@ -14,9 +14,61 @@ const unlinkFile = (path) => new Promise((resolve, reject) => {
 	})
 });
 
-const rp = ({ id }, formData) => new Promise((resolve, reject) => {
-	
+const absolutePath = (path) => path.match(/(\w*).{1,}/)[1];
 
+const writeMetaFile = (path, file) => new Promise((resolve, reject) => {
+	fs.writeFile(path, file, err => {
+		if (err) reject("Can't create meta file")
+		resolve();
+	})
+});
+
+const getStat = (src) => new Promise((resolve, reject) => {
+		fs.stat(src, (err, stats) => {
+			if (err) reject("Can't get stat on this file");
+			resolve(stats);
+		});
+});
+
+const getMetaDataFile = async ({ src, c, dest }) => {
+	console.log('get Stat');
+	const stats = await getStat(src);
+	console.log('stats = ', stats)
+	const compress = (c) ? true : false;
+	const date = new Date().toLocaleString();
+	const checksum = md5file.sync(src);
+	const metadata = `const metadata = {\n\tname: '${dest}',\n\tchecksum: '${checksum}',\n\tcompress: '${compress}',\n\tsize: '${stats.size}',\n\tsource: '${src}',\n\tdate : '${date}'\n}\n\nmodule.exports = metadata;`;
+	console.log('then meta');
+
+	return metadata;
+};
+
+const createDir = (dirname) => new Promise((resolve, reject) => {
+	mkdirp(dirname, err => {
+		if (err) reject(`Can't create directory ${dirname}`)
+		resolve();
+	})
+});
+
+const createDirAndWriteMetafile = async ({ dest }, dirname, metadata) => {
+	console.log('create dir temp');
+	await createDir(dirname)
+	await writeMetaFile(`${dirname}${absolutePath(dest)}.js`, metadata)
+};
+
+const createZipFile = async ({ src, dest, m }, newZip, pathMeta) => {
+		const output = fs.createWriteStream(newZip);
+		const archive = archiver(newZip, {
+		  zlib: { level: 9 }
+		});
+		archive.append(fs.createReadStream(src), { name: dest });
+		if (m) archive.append(fs.createReadStream(`${pathMeta}${absolutePath(dest)}.js`), { name: 'meta-' + absolutePath(dest) + '.js' });
+		archive.pipe(output);
+		await archive.finalize().catch(() => reject(`Can't create archive`));
+		return;
+};
+
+const rp = async ({ id }, formData) => {
 	const token = jwt.sign({ foo: 'bar' }, 'shhhhh');
 	console.log(token)
 	const r = request.defaults({
@@ -28,116 +80,63 @@ const rp = ({ id }, formData) => new Promise((resolve, reject) => {
 		formData: formData
 	});
 
-	r.post(`http://178.62.77.158?id=${id}`, (err, http, body) => {
-		console.log('request');
-		if (err) reject(`Error rp(): ${err}`)
-		resolve();
-	}).auth('dave', 'dav');
-});
+	await new Promise((resolve, reject) => {
+		r.post(`http://178.62.77.158:5000/upload?id=${id}`, (err, http, body) => {
+			console.log('request');
+			if (err) reject(`Error rp(): ${err}`)
+			resolve();
+		}).auth('dave', 'dav');
+	});
+};
 
-const absolutePath = (path) => path.match(/(\w*).{1,}/)[1];
-
-const writeFile = (path, file) => new Promise((resolve, reject) => {
-	fs.writeFile(path, file, err => {
-		if (err) reject("Can't create meta file")
-		resolve();
-	})
-});
-
-const createDirAndWriteMetafile = ({ dest }, dirname, metadata) => new Promise((resolve, reject) => {
-	console.log('create dir temp');
-	mkdirp(dirname, err => {
-		if (err) reject(`Can't create directory ${dirname}`)
-		console.log('create metafile')
-		writeFile(`${dirname}${absolutePath(dest)}.js`, metadata)
-		.then(() => resolve()).catch(err => reject(err))
-	})
-});
-
-const getStat = ({ src, c, dest }) => new Promise((resolve, reject) => {
-	console.log('get Stat');
-	fs.stat(src, (err, stats) => {
-		if (err) reject("Can't get stat on this file")
-
-		const compress = (c) ? true : false;
-		const date = new Date().toLocaleString();
-		const checksum = md5file.sync(src);
-		const metadata = `const metadata = {\n\tname: '${dest}',\n\tchecksum: '${checksum}',\n\tcompress: '${compress}',\n\tsize: '${stats.size}',\n\tsource: '${src}',\n\tdate : '${date}'\n}\n\nmodule.exports = metadata;`;
-		console.log('then meta');
-
-		resolve(metadata);
-	})
-});
-
-const checkIfMetaOptionExist = (object) => new Promise((resolve, reject) => {
-	console.log(`check meta`);
-	if (object.m) getStat(object).then(metadata => resolve(metadata)).catch(err => reject(err))
-	else reject();
-});
-
-const createZip = ({ src, dest, m }, newZip, pathMeta) => new Promise((resolve, reject) => {
-		const output = fs.createWriteStream(newZip);
-		const archive = archiver(newZip, {
-		  zlib: { level: 9 }
-		});
-		archive.append(fs.createReadStream(src), { name: dest });
-		if (m) archive.append(fs.createReadStream(`${pathMeta}${absolutePath(dest)}.js`), { name: 'meta-' + absolutePath(dest) + '.js' });
-		archive.pipe(output);
-		archive.finalize().then(() => resolve()).catch(() => reject(`Can't create archive`));
-});
-
-const checkIfCompressOptionExist = (object, newZip, pathToGetMetaFile) => new Promise((resolve, reject) => {
-	if (object.c) createZip(object, newZip, pathToGetMetaFile).then(() => resolve()).catch(err => reject(err));
-	else reject();
-});
-
-const upload = (object) => {
+const upload = async (object) => {
 
 	const newZip = (object.c) ? absolutePath(object.dest) + '.zip' : false;
 	const pathToGetMetaFile = '/tmp/metaTemp/';
 
-	checkIfMetaOptionExist(object)
-	.then(metadata => createDirAndWriteMetafile(object, pathToGetMetaFile, metadata))
-	.catch(err => {
-		if (err) console.error(err)
-	})
-	.finally(() => checkIfCompressOptionExist(object, newZip, pathToGetMetaFile))
-	.then(() => {
-		console.log('then compress')
-		const formData = {
-			filename: (object.c) ? newZip : object.dest,
-			absolutename: absolutePath(object.dest),
-			meta: (object.m) ? 'true' : '',
-			file: (object.c) ? fs.createReadStream(newZip) : fs.createReadStream(object.src),
-		};
-		rp(object, formData).then(() => {
+	if (object.m) {
+		try {	
+			const metadata = await getMetaDataFile(object);
+			await createDirAndWriteMetafile(object, pathToGetMetaFile, metadata);
+		}
+		catch (e) {console.error(e)}
+	}
+	if (object.c) {
+		try {
+			await createZipFile(object, newZip, pathToGetMetaFile)
+			const formData = {
+				filename: (object.c) ? newZip : object.dest,
+				absolutename: absolutePath(object.dest),
+				meta: (object.m) ? 'true' : '',
+				file: (object.c) ? fs.createReadStream(newZip) : fs.createReadStream(object.src),
+			};
+			await rp(object, formData).catch(console.error)
 			if (object.m) unlinkFile(`${pathToGetMetaFile}${absolutePath(object.dest)}.js`)
 			if (newZip) unlinkFile(newZip)
-		})
-		.catch(console.error)
-	})
-	.catch(() => {
-		const formData = {
-			filename: (object.c) ? newZip : object.dest,
-			absolutename: absolutePath(object.dest),
-			meta: (object.m) ? 'true' : '',
-			file: (object.c) ? fs.createReadStream(newZip) : fs.createReadStream(object.src),
-		};
-		console.log(formData);
-		rp(object, formData).then(() => {
-			if (newZip) unlinkFile(newZip)
-		})
-		.catch(console.error);
-
+		}
+		catch (e) {console.error(e)}
+	}
+	else {
 		if (object.m) {
 			const formData = {
 					filename: absolutePath(object.dest) + '.js',
 					src: pathToGetMetaFile,
 					file: fs.createReadStream(`${pathToGetMetaFile}${absolutePath(object.dest)}.js`),
 			};
-			rp(object, formData).then(() => unlinkFile(`${pathToGetMetaFile}${absolutePath(object.dest)}.js`)).catch(console.error);
+			console.log('1')
+			await rp(object, formData).catch(console.error);
+			console.log('1')
+			unlinkFile(`${pathToGetMetaFile}${absolutePath(object.dest)}.js`)
 		}
-	})
+		const formData = {
+			filename: (object.c) ? newZip : object.dest,
+			absolutename: absolutePath(object.dest),
+			meta: (object.m) ? 'true' : '',
+			file: (object.c) ? fs.createReadStream(newZip) : fs.createReadStream(object.src),
+		};
+		await rp(object, formData).catch(console.error)
+		if (newZip) unlinkFile(newZip)
+	}
 };
 
 const download = ({ src, dest }) => new Promise((resolve, reject) => {
