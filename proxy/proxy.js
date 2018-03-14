0f37e3d1cf7960d3ea4979a652448e6d2c59a955
Lu <lubaujar@student.42.fr>
2018-03-14 15:30:38 +0100

    Update proxy.js

--- proxy/proxy.js
+++ proxy/proxy.js
@@ -2,7 +2,7 @@ const http = require('http');
 const request = require('request');
 
 
-
+//                               FUNCTION ??????? =>
 const proxy = http.createServer(function(req, res) {
   req.headers.proxy = true;
   req.pipe(request(req.url)).pipe(res);
